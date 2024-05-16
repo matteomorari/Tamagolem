de2306d6d5cdc30a2e0a1f9c2b8c39cd52d8e308
Matteo <matteo.morari04@gmail.com>
2024-05-16 15:52:14 +0200

    end WorldBalance

--- src/it/unibs/PgAr/Tamagolem/TamagolemApp.java
+++ src/it/unibs/PgAr/Tamagolem/TamagolemApp.java
@@ -5,6 +5,6 @@ import it.unibs.PgAr.Tamagolem.Graph.Graph;
 public class TamagolemApp {
     public static void main(String[] args) throws Exception {
         Graph graph = new Graph(4);
-        System.out.println();
+        System.out.println(graph.toString());
     }
 }
